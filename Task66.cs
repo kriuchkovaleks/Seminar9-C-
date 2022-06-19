bfb7041e8488d48e9ef3fcafd40dc4d4b7a242ec
kriuchkovaleks <103981431+kriuchkovaleks@users.noreply.github.com>
2022-06-19 16:03:36 +0300

    Add files via upload

---
+++ Task66.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+
+
+
+
+int GetNaturalSum(int M, int N) // commit
+{
+    if (M == N) return M;
+    
+    return GetNaturalSum(M, N -1) + N;
+       
+}
+
+Console.WriteLine("Input M");
+int M = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Input N");
+int N = int.Parse(Console.ReadLine());
+int summ = 0;
+
+int result = GetNaturalSum(M, N);
+
+Console.WriteLine(result);
---
+++ Task68.cs
@@ -0,0 +1,36 @@
+﻿//// Task 68.  Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.Clear();
+Console.WriteLine("This program solves Ackermann's function.");
+Console.WriteLine();
+int numberM = EnterUserData("Enter the start number M:");
+int numberN = EnterUserData("Enter the end number N:");
+if (numberM < 0 || numberN < 0)
+{
+    Console.WriteLine("Please enter positive numbers m and n.");
+    return;
+}
+int result = AckermannFunction(numberM, numberN);
+Console.WriteLine(result);
+
+int AckermannFunction(int numberM, int numberN)
+{
+    if (numberM == 0)
+    {
+        return numberN + 1;
+    }
+    else if (numberM > 0 && numberN == 0)
+    {
+        return AckermannFunction(numberM - 1, 1);
+    }
+    else if (numberM > 0 && numberN > 0)
+    {
+        return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+    }
+    return AckermannFunction(numberM, numberN);
+}
+
+int EnterUserData(string nameUserData)
+{
+    Console.Write($"{nameUserData}");
+    return Convert.ToInt32(Console.ReadLine());
+}
